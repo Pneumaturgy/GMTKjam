739136526158238ca6e0bdd0fec82bb6969a18b8
Dylan Growcoot <93146289+Ghigog@users.noreply.github.com>
2024-08-19 08:56:26 -0500

    Trink generation update

--- Scripts/TrinkScripts/HazardTrink.gd
+++ Scripts/TrinkScripts/HazardTrink.gd
@@ -3,9 +3,5 @@ class_name HazardTrink
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var sides = [
-		0,
-		0,
-		0
-	]
+	no_spawn_growth_factor = 10
 	super._ready()
--- Scripts/TrinkScripts/Trink.gd
+++ Scripts/TrinkScripts/Trink.gd
@@ -1,14 +1,18 @@
 extends Node2D
 class_name Trink
 
+@onready var label = $Label
+var ray_cast_2d : RayCast2D
 
 var no_spawn_chance = 50
 var no_spawn_growth_factor = 1.5 # 20%
 var rarity : float
-var size : int
+var size = 10
+var small_trink_size = 10
+var parent_trink = false
 
 
-var siblings = {
+var sides = {
 	0: null, # This is the right side
 	1: null, # This is the left side
 	2: null # This is the base, or bottom side
@@ -16,23 +20,70 @@ var siblings = {
 
 
 func _ready():
-	for father_side in siblings:
-		if siblings[father_side] != null: continue # Don't replace the parent trink
+	size = small_trink_size
+	for side in sides:
+		
+		## Check if it is possible to spawn a new trink
+		if parent_trink and side == 2: continue # If a child trink, don't spawn anything directly below
+		if sides[side] != null: continue # Don't replace an existing trink
+		if await check_for_obstacles(): continue
+		print("we all good for spawning trinks chief")
+		
+		## Let's find a new trink
 		var new_trink_type = choose_new_trink(no_spawn_chance)
 		if new_trink_type == null: continue # No new trinks on this side
+		print('new trink type: ', new_trink_type)
+		await get_tree().create_timer(1).timeout
+		
+		## Load the trink
 		var new_trink = load(new_trink_type).instantiate()
 		new_trink.no_spawn_chance = no_spawn_chance * no_spawn_growth_factor
-		# TODO Need to SET on the new_trink.siblings the father on the correct side
-		# So, for instance: new_trink.siblings[get_sibling_side(father_side)] = self
-		# TODO Need to fix the get new transform function (don't hardcode but calculate the coordinates)
-		new_trink.transform = get_new_transform(father_side)
+		# update the spawn chance
+		new_trink.sides[get_sibling_side(side)] = self 
+		# I set myself as one of the existing siblings for the next trink
+		print('new trink type: ', new_trink, " new spawn chance: ", new_trink.no_spawn_chance)
+		await get_tree().create_timer(1).timeout
+		
+		## Position the Trink
+		var new_transform = get_new_transform(side)
+		new_trink.position = new_transform[0]
+		new_trink.rotation = deg_to_rad(new_transform[1])
+		print('new trink transform: ', new_trink.position, " ", new_trink.rotation)
+		await get_tree().create_timer(1).timeout
+		
+		## Update variables
+		label.text = (str(Global.index))
+		Global.index += 1
+		sides[side] = new_trink
+		new_trink.parent_trink = true
 		add_child(new_trink)
-		siblings[father_side] = new_trink
+		print("new trink! ", new_trink)
+		await get_tree().create_timer(1).timeout
+		
+		
+		
 
+func check_for_obstacles():
+	ray_cast_2d = RayCast2D.new()
+	ray_cast_2d.target_position = Vector2(0,size+1)
+	add_child(ray_cast_2d)
+	#ray_cast_2d.add_exception(get_node("TrinkBody"))
+	for side in sides.size():
+		if ray_cast_2d.is_colliding():
+			return ray_cast_2d.is_colliding()
+			print("hit: ",ray_cast_2d.get_collider())
+			ray_cast_2d.queue_free()
+		else:
+			await get_tree().create_timer(.5).timeout
+			print(ray_cast_2d.get_collider())
+			ray_cast_2d.rotation += deg_to_rad(120)
+	print("hit nothing: ",ray_cast_2d.get_collider())
+	ray_cast_2d.queue_free()
+	return ray_cast_2d.is_colliding()
 
-func choose_new_trink(no_spawn_chance):
+func choose_new_trink(chance):
 	var rng = RandomNumberGenerator.new()
-	var num = rng.randi_range(1, Global.total_spawn_chance + no_spawn_chance)
+	var num = rng.randi_range(1, Global.total_spawn_chance + chance)
 	for path in Global.all_trinks:
 		num -= Global.all_trinks[path]
 		if num <= 0:
@@ -41,17 +92,35 @@ func choose_new_trink(no_spawn_chance):
 
 
 func get_new_transform(side):
-	var new_transform
+	#var offset = size /2 * sqrt(3)
+	#print("current offset: ", offset)
+	#var new_transform
+	var new_position
+	var new_rotation
 	if side == 0:
-		var new_position = Vector2(11.75,-7.5)
-		var new_rotation = self.rotation + 60.0
-		new_transform = Transform2D(new_rotation, new_position)
+		print("right")
+		new_position = get_parent().position + Vector2(size, (-size/2 - 1))
+		new_rotation = get_parent().rotation + 60.0
 	elif side == 1:
-		var new_position = Vector2(-11.75,-7.5)
-		var new_rotation = self.rotation + 60.0
-		new_transform = Transform2D(new_rotation, new_position)
+		print("left")
+		new_position = get_parent().position + Vector2(-size, (-size/2 - 1))
+		new_rotation = get_parent().rotation - 60.0
 	elif side >= 2: 
-		var new_position = Vector2(0,11.75)
-		var new_rotation = self.rotation + 60.0
-		new_transform = Transform2D(new_rotation, new_position)
-	return new_transform
+		print("down")
+		new_position = get_parent().position + Vector2(0,size+1)
+		new_rotation = get_parent().rotation + 180
+	#new_transform = Transform2D(new_rotation, new_position)
+	print(new_rotation, ' new rotation ', new_position, ' new position ')
+	return [new_position,new_rotation]
+
+	
+
+	
+func get_sibling_side(side):
+	if side == 0:
+		return 1
+	elif side == 1:
+		return 0
+	elif side == 2:
+		return 2
+	
