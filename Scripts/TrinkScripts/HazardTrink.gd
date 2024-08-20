91752b9ca9f93ab845409e078d305c1d6ab99d50
Dylan Growcoot <93146289+Ghigog@users.noreply.github.com>
2024-08-19 20:54:00 -0500

    new trink improvements

--- Scripts/TrinkScripts/HazardTrink.gd
+++ Scripts/TrinkScripts/HazardTrink.gd
@@ -4,5 +4,4 @@ class_name HazardTrink
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	no_spawn_growth_factor = 10
-	no_spawn_chance = 100
 	super._ready()
--- Scripts/TrinkScripts/Trink.gd
+++ Scripts/TrinkScripts/Trink.gd
@@ -4,9 +4,9 @@ class_name Trink
 @onready var label = $Label
 var ray_cast_2d : RayCast2D
 
-@onready var right_slot = $RightSlot
-@onready var left_slot = $LeftSlot
-@onready var down_slot = $DownSlot
+var right_slot
+var left_slot
+var down_slot
 
 var sides = {
 	0: null, # This is the right side
@@ -20,31 +20,39 @@ var rarity : float
 var size = 200
 var small_trink_size = 200
 var parent_trink = false
-
+var can_trink = true
 
 
 
 
 func _ready():
+	right_slot = get_node_or_null("RightSlot")
+	left_slot = get_node_or_null("LeftSlot")
+	down_slot = get_node_or_null("DownSlot")
 	size = small_trink_size
 	for side in sides:
+		if !can_trink: continue
 		## Check if it is possible to spawn a new trink
 		if parent_trink and side == 2: continue # If a child trink, don't spawn anything directly below
 		if sides[side] != null: continue # Don't replace an existing trink
+		print(get_slot(side).has_overlapping_bodies())
 		if get_slot(side).has_overlapping_bodies(): continue #skip this side if it's colliding
 		#if await check_for_obstacles(): continue
 		print("we all good for spawning trinks chief")
 		
 		## Let's find a new trink
-		print("looking for trinks...")
+		#print("looking for trinks...")
 		var new_trink_type = choose_new_trink(no_spawn_chance)
 		if new_trink_type == null: continue # No new trinks on this side
-		print('new trink type: ', new_trink_type)
+		#print('new trink type: ', new_trink_type)
 		await get_tree().create_timer(.25).timeout
 		
 		## Load the trink
 		var new_trink = load(new_trink_type).instantiate()
-		new_trink.no_spawn_chance = no_spawn_chance * no_spawn_growth_factor
+		if new_trink_type != "res://Scenes/Trinks/BlankTrink.tscn":
+			new_trink.can_trink = false # if it's a hazard, it shouldn't spawn anything else after it
+		else:
+			new_trink.no_spawn_chance = no_spawn_chance * no_spawn_growth_factor
 		# update the spawn chance
 		new_trink.sides[get_sibling_side(side)] = self 
 		# I set myself as one of the existing siblings for the next trink
@@ -55,7 +63,7 @@ func _ready():
 		#var new_transform = get_new_transform(side)
 		#new_trink.position = new_transform[0]
 		#new_trink.rotation = deg_to_rad(new_transform[1])
-		print('new trink transform: ', new_trink.position, " ", new_trink.rotation)
+		#print('new trink transform: ', new_trink.position, " ", new_trink.rotation)
 		await get_tree().create_timer(.25).timeout
 		
 		## Update variables
@@ -64,36 +72,18 @@ func _ready():
 		sides[side] = new_trink
 		new_trink.parent_trink = true
 		get_slot(side).add_child(new_trink)
-		print("new trink! ", new_trink)
+		#print("new trink! ", new_trink)
 		await get_tree().create_timer(.25).timeout
 		
-#func check_for_obstacles():
-	#ray_cast_2d = RayCast2D.new()
-	#ray_cast_2d.target_position = Vector2(0,size+1)
-	#add_child(ray_cast_2d)
-	##ray_cast_2d.add_exception(get_node("TrinkBody"))
-	#for side in sides.size():
-		#if ray_cast_2d.is_colliding():
-			#return ray_cast_2d.is_colliding()
-			#print("hit: ",ray_cast_2d.get_collider())
-			#ray_cast_2d.queue_free()
-		#else:
-			#await get_tree().create_timer(.5).timeout
-			#print(ray_cast_2d.get_collider())
-			#ray_cast_2d.rotation += deg_to_rad(120)
-	#print("hit nothing: ",ray_cast_2d.get_collider())
-	#ray_cast_2d.queue_free()
-	#return ray_cast_2d.is_colliding()
 
 func choose_new_trink(chance):
-	
 	var rng = RandomNumberGenerator.new()
 	var num = rng.randi_range(1, Global.total_spawn_chance + chance)
-	print("chance is: ", chance, " and I got: ", num)
+	#print("chance is: ", chance, " and I got: ", num)
 	for path in Global.all_trinks:
 		num -= Global.all_trinks[path]
 		if num <= 0:
-			print('im trinkin ', path)
+			#print('im trinkin ', path)
 			return path
 	return null
 
